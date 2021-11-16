642492df7b83a695da1c7dfc61294f81f9f99def
LiangYaHui <46517115@qq.com>
2021-11-16 16:00:22 +0800

    更新一下

--- benchmark.js
+++ benchmark.js
@@ -1,44 +1,85 @@
-let now = Date.now()
-let data = Date.now()
+const { uuid } = require('./index')
 
-for (let i = 0; i < 10000 * 10000; i++) {
-  data.toString(16)
+function uuid1() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0,
+          v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+  });
 }
-console.log(Date.now() - now)
-function trans(x) {
 
-}
-now = Date.now()
-let n = data;
-let m = []
-for (let i = 0; i < 10000 * 10000; i++) {
-  while(n > 0) {
-    let a = n % 16;
-    n = (n - a)/16;
-    switch(a) {
-      case 10:
-        m.push('A');
-        break;
-      case 11:
-        m.push('B');
-        break;
-      case 12:
-        m.push('C');
-        break;
-      case 13:
-        m.push('D');
-        break;
-      case 14:
-        m.push('E');
-        break;
-      case 15:
-        m.push('F');
-        break;
-      default:
-        m.push(a + '')
-    }
+function uuid2() {
+  var s = [];
+  var hexDigits = "0123456789abcdef";
+  for (var i = 0; i < 36; i++) {
+      s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
   }
-}
-console.log(Date.now() - now)
+  s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+  s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+  s[8] = s[13] = s[18] = s[23] = "-";
 
-console.log(data.toString(16), m.reverse().join(''))
+  var uuid = s.join("");
+  return uuid;
+}
+
+function uuid3() {
+  function S4() {
+      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  }
+  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
+
+function uuid4(len, radix) {
+  var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+  var uuid = [],
+      i;
+  radix = radix || chars.length;
+
+  if (len) {
+      for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random() * radix];
+  } else {
+      var r;
+
+      uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+      uuid[14] = '4';
+
+      for (i = 0; i < 36; i++) {
+          if (!uuid[i]) {
+              r = 0 | Math.random() * 16;
+              uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+          }
+      }
+  }
+
+  return uuid.join('');
+}
+
+let now = Date.now()
+for (let i = 0; i < 10000 * 10000; i++) {
+  uuid()
+}
+console.log('uuid', Date.now() - now, uuid())
+
+now = Date.now()
+for (let i = 0; i < 10000 * 10000; i++) {
+  uuid1()
+}
+console.log('uuid1', Date.now() - now, uuid1())
+
+now = Date.now()
+for (let i = 0; i < 10000 * 10000; i++) {
+  uuid2()
+}
+console.log('uuid2', Date.now() - now, uuid2())
+
+now = Date.now()
+for (let i = 0; i < 10000 * 10000; i++) {
+  uuid3()
+}
+console.log('uuid3', Date.now() - now, uuid3())
+
+now = Date.now()
+for (let i = 0; i < 10000 * 10000; i++) {
+  uuid4(16, 16)
+}
+console.log('uuid4', Date.now() - now, uuid4(16,16))
